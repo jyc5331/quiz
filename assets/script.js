2f2bb8108128e3cd69ad23eb5d761d5ec48b67ed
jyc5331 <jcosgrovecoding@gmail.com>
2020-11-20 16:37:24 -0500

    added comments to JS and CSS, stylized header

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// constant variables
+
+// get document elements
+
+// quiz questions and make sure that correct or wrong displays onclick
+
+// timer
+
+// calls 
+
+// event listeners
+
+// high score to local storage
