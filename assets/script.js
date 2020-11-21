300a1e82701b170c4d63b62a86d563f400bab528
jyc5331 <jcosgrovecoding@gmail.com>
2020-11-21 18:26:41 -0500

    questions finished and extensive commenting added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // global variables and document elements
 var timerEl = document.getElementById('timer');
-var startBtn = document.getElementById('start');
+var startBtn = document.getElementById('theStartButton');
 var quizContainer = document.getElementById('quizContainer');
 var highScoreText = document.getElementById('highScoreText');
 
@@ -52,20 +52,29 @@ const questions = [
     answers: {
       a: "Incorporating inline in HTML",
       b: "Linking to an external style sheet",
-      c: "",
-      d: "ESLint"
+      c: "The ancient and time honored ritual of augury",
+      d: "There is no current best practice"
     },
     correctAnswer: "b"
   }
 ];
 // display quiz questions and next button, make next button display whether answer was right or wrong and subtract 10 from timer
+// at the end of the quiz, take remaining time and return the value so highScore can set to localStorage
 
 */
 
 // Actual quiz function. HELP how to run questions...for loop? idea is to replace quizContainer content with a new explanationText and 4 buttons 
 function quizBegin() {
 alert ("quiz is beginning");
-}
+// loop through the questions with: for(var i=0; i <questions.length; i++){
+// var response = HELP something to indicate that the question has been answered
+// if (response == questions[i]. answer){
+    //display Correct! and switch slide to next question
+// else { display Wrong! text and subtract 10 from timeLeft}
+
+//} end of if statement
+//} end of loop
+} //end of quizBegin function
 
 // calls 
 // HELP this stopped working when I had both functions running on the same button startBtn.onclick = countdown;
