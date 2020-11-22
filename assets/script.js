2d0bea352e2f23d95bcf23b43a3bcc55ac34d56c
jyc5331 <jcosgrovecoding@gmail.com>
2020-11-22 10:15:02 -0500

    return timeLeft and more fiddling with click event for start button

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var quizContainer = document.getElementById('quizContainer');
 var highScoreText = document.getElementById('highScoreText');
 var questionSlide = document.getElementById('questionSlide');
 var highScoreSlide = document.getElementById('highScoreSlide');
-var highScore = [];
+var highScores = [];
 
 
 // timer, starts at 60, displays in 'timer' element, is affected by quizBegin function
@@ -26,10 +26,10 @@ var timeInterval = setInterval(function() {
       displayMessage();
     }
   }, 1000);
+  return timeLeft;
 }
 
 
-/* Quiz questions and answers: 
 const questions = [
   {
     question: "Select which variable uses the best-practice for casing in JavaScript:",
@@ -46,7 +46,7 @@ const questions = [
     answers: {
       a: "Document Object Map",
       b: "Document Obliterate Malfeasance",
-      c: "Document Object Noodle"
+      c: "Document Object Noodle",
       d: "Document Object Model"
     },
     correctAnswer: "d"
@@ -56,24 +56,23 @@ const questions = [
     answers: {
       a: "Incorporating inline in HTML",
       b: "Linking to an external style sheet",
-      c: "The ancient and time honored ritual of augury",
+      c: "The ancient, time honored ritual of augury",
       d: "There is no current best practice"
     },
     correctAnswer: "b"
   }
 ];
-// display quiz questions and next button, make next button display whether answer was right or wrong and subtract 10 from timer
-// at the end of the quiz, take remaining time and return the value so highScore can set to localStorage
 
-*/
 
 // Actual quiz function. HELP how to run questions...for loop? idea is to replace quizContainer content with a new explanationText and 4 buttons 
 function quizBegin() {
 alert ("quiz is beginning");
+// HELP uncaught property quizContainer.style.display = "none" ;
+
 // loop through the questions with: for(var i=0; i <questions.length; i++){
 // var response = HELP something to indicate that the question has been answered
-// if (response == questions[i]. answer){
-    //display Correct! in some form of feedback section in quizContainer and switch slide to next question
+// if (response == questions[i]. correctAnswer){
+    //writing feedback: document.getElementById("feedback").innerHTML =Correct or Wrong!
 // else { display Wrong! text and subtract 10 from timeLeft}
 
 //} end of if statement
@@ -82,12 +81,11 @@ alert ("quiz is beginning");
 } //end of quizBegin function
 
 // calls 
-// HELP this stopped working when I had both functions running on the same button startBtn.onclick = countdown;
+startBtn.onclick = countdown;
 startBtn.onclick = quizBegin;
 highScoreText.onclick = displayHighScores;
 
-// high score to local storage. make numbered list of high scores in order of highest-lowest. 
-// set information in key, value format to local storage, get information on command and have on a slide
+// get high score from localStorage, append the values to individual <li> within highScoreSlide
 // have the active page disappear and highScoreSlide display when the user clicks "highScoreText"
 function displayHighScores() {
     alert ('heyo');
